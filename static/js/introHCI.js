d55349c68725bb4407c141b8f4f23db1d2aa032c
Eric Yang <ery006@ucsd.edu>
2019-01-23 15:04:15 -0800

    first part

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,30 @@
-'use strict';
-
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
-
+function projectClick(e) { 
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").fadeOut();
+    }
+}
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").toggleClass("active");
 	});
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
